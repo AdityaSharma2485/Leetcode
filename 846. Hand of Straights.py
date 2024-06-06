51596038f56a5dd7af88c259ba54416179f68be6
Aditya Sharma <92670331+AdityaSharma2485@users.noreply.github.com>
2024-06-06 08:44:39 +0530

    Create 846. Hand of Straights.py
    Leetcode DPP (Medium)

---
+++ 846. Hand of Straights.py
@@ -0,0 +1,19 @@
+from collections import Counter
+from typing import List
+
+class Solution:
+    def isNStraightHand(self, nums: List[int], k: int) -> bool:
+        if len(nums) % k != 0:
+            return False
+        
+        num_count = Counter(nums)
+        sorted_keys = sorted(num_count.keys())
+        
+        for num in sorted_keys:
+            while num_count[num] > 0:
+                for i in range(k):
+                    if num_count[num + i] > 0:
+                        num_count[num + i] -= 1
+                    else:
+                        return False
+        return True
