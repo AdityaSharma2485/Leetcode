5d3672ff1b7b6f7712c77150e5d2c17458d63ab4
Aditya Sharma <92670331+AdityaSharma2485@users.noreply.github.com>
2024-06-17 11:31:50 +0530

    Create 633. Sum of Square Numbers.py
    Leetcode DPP (Medium)

---
+++ 633. Sum of Square Numbers.py
@@ -0,0 +1,22 @@
+import math
+
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        if c < 0:
+            return False
+        
+        left = 0
+        right = int(math.sqrt(c))
+        
+        while left <= right:
+            current_sum = left * left + right * right
+            if current_sum == c:
+                return True
+            elif current_sum < c:
+                left += 1
+            else:
+                right -= 1
+        
+        return False
+
+        
